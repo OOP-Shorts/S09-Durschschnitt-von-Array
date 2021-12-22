9e1c989f5fd566cdc2eb6488594ff44d9d4ddb31
ragor114 <69634980+ragor114@users.noreply.github.com>
2021-12-22 10:44:18 +0100

    added solution

--- src/ArrayAverage.java
+++ src/ArrayAverage.java
@@ -3,17 +3,38 @@ public class ArrayAverage {
     /* Schreiben Sie eine Methode, die ein int[] values übergeben bekommt und den Durschnitt aller Werte zurückgibt.
     Lagern Sie dabei das Aufsummieren der Werte in eine eigene Methode aus.*/
     public static int intArrayAverageWithMethod(int[] values) {
-        return 0;
+        // Ist das values Array leer wird 0 zurückgegeben.
+        if(values == null || values.length == 0) {
+            return 0;
+        }
+
+        // Der Durchschnitt ist die Summe aller Element geteilt durch die Zahl der Elemente.
+        // Die Zahl der Elemente entspricht der Länge des Arrays.
+        int average = sumValues(values) / values.length;
+
+        return average;
+    }
+
+    /*
+        Diese Methode berechnet die Summe aller Elemente im Array.
+        Dazu wird das gesamte Array mit einer for-Schleife durchlaufen und jedes Element des Arrays auf sum addiert.
+     */
+    public static int sumValues(int[] values) {
+        int sum = 0;
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i];
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
         int[] testArray = {2, 5, 7, 8, 9, 1, 6, 3, 4, 0};
-        int testSum = intArrayAverageWithMethod(testArray);
+        int testAverage = intArrayAverageWithMethod(testArray);
 
-        if(testSum == 4) {
+        if(testAverage == 4) {
             System.out.print("Correct!");
         } else {
-            System.out.println("Your solution was " + testSum + " but 4 is correct.");
+            System.out.println("Your solution was " + testAverage + " but 4 is correct.");
         }
     }
 }
